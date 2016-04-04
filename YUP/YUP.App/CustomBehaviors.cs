cb0813b51757e52f257b866cee9ff3fe815669d6
RafPe <rafal@pieniazek.nl>
2016-04-04 19:21:17 +0200

    Added custom behaviors class

---
+++ YUP/YUP.App/CustomBehaviors.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace YUP.App
+{
+    public static class CustomBehaviors
+    {
+        //
+        public static string GetLoadedMethodName(DependencyObject obj)
+        {
+            return (string)obj.GetValue(LoadedMethodNameProperty);
+        }
+
+        public static void SetLoadedMethodName(DependencyObject obj, string value)
+        {
+            obj.SetValue(LoadedMethodNameProperty, value);
+        }
+
+        public static readonly DependencyProperty LoadedMethodNameProperty =
+            DependencyProperty.RegisterAttached("LoadedMethodName",
+            typeof(string), typeof(CustomBehaviors), new PropertyMetadata(null, OnLoadedMethodNameChanged));
+
+        private static void OnLoadedMethodNameChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            FrameworkElement element = d as FrameworkElement;
+            if (element != null)
+            {
+
+                element.Loaded += (s, e2) =>
+                {
+                    var viewModel = element.DataContext;
+                    if (viewModel == null) return;
+                    var methodInfo = viewModel.GetType().GetMethod(e.NewValue.ToString());
+                    if (methodInfo != null) methodInfo.Invoke(viewModel, null);
+                };
+            }
+        }
+
+
+    }
+}
